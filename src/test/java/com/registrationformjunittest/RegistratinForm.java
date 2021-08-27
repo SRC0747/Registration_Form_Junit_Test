e1c3d3a3eeaf6cf626eb7ad7480c23f5a9a2efe5
SRC0747 <RjSrc@0747>
2021-08-28 00:34:46 +0530

    Registration Form

---
+++ src/test/java/com/registrationformjunittest/RegistratinForm.java
@@ -0,0 +1,8 @@
+package com.registrationformjunittest;
+
+public class RegistratinForm {
+    public static void main(String[] args)
+    {
+        System.out.println("Registration Form");
+    }
+}
